119ccca8828b7225b5758d425626593dd14e4b2e
Jeremy <1025024131@qq.com>
2017-11-01 12:31:09 +0800

    完善舆情分析-趋势分析模块-Jeremy-2017年11月1日 12：30

--- app/controller/DataAnalysis.php
+++ app/controller/DataAnalysis.php
@@ -155,33 +155,105 @@ class DataAnalysis extends Common
      */
     public function getTrendLine(){
         $params = input('post.');
+        $obj = input('post.obj', '');
         $task_id = input('post.task_id', -1);
-        $timeRange = input('post.timeRange', -1);
+        $stime = input('post.begintime_str', '');
+        $etime = input('post.endtime_str', '');
         /**
          * timeRange: 0->最近24小时
          *            1->最近7天
          *            2->最近30天
          */
-        $ret = ['errorcode' => 0,'data' => [], 'msg' => ''];
+        $ret = ['errorcode' => 0, 'msg' => ''];
         $cond = [];
         if($task_id == -1){
             $task_id = 3; //这里为测试，实际上要获取task表中最后一条有效数据的id
         }
-        if($timeRange == 1){
 
+        /**
+         * 参考代码
+         *     // 如果开始时间和结束时间相同，则为一天（今天、昨天或自定义某一天）
+        if ($days == 0) {
+        for ($i = 0; $i <= 23; $i++) {
+        $hour = $i;
+        if ($i <= 9)
+        $hour = '0' . $i;
+        $datetime_hour = $beginTime . ' ' . $hour;
+
+        // get time
+        $sqlTime = "select sum(time_len) as time from statistic_view where mer_id= {$mer_id} and consume_t like '{$datetime_hour}%'";
+        $time = $adminDB->ExecSQL($sqlTime, $conn);
+        array_push($return['time'], $time[0]['time'] == null ? 0 : $time[0]['time']);
+
+        // get money
+        $sqlMoney = "select sum(cost) as money from statistic_view where mer_id = {$mer_id}  and consume_t like '{$datetime_hour}%'";
+        $money = $adminDB->ExecSQL($sqlMoney, $conn);
+        array_push($return['money'], $money[0]['money'] == null ? 0 : $money[0]['money']);
+
+        // set xAxis
+        array_push($return['xAxis'], ($i <= 9 ? ('0' . $i) : $i) . ':00');
         }
 
-        $ret['timeRange'] = ['周一','周二','周三','周四','周五','周六','周日'];
+        } else if ($days <= 31) {
+        for ($i = 0; $i <= $days; $i++) {
+        $datetime = date('Y-m-d', strtotime($beginTime . '+' . $i . 'days'));
+
+        // get time
+        $sqlTime = "select sum(time_len) as time from statistic_view where mer_id = {$mer_id}  and consume_t like '{$datetime}%'";
+        $time = $adminDB->ExecSQL($sqlTime, $conn);
+        array_push($return['time'], $time[0]['time'] == null ? 0 : $time[0]['time']);
+
+        // get money
+        $sqlMoney = "select sum(cost) as money from statistic_view where mer_id = {$mer_id}  and consume_t like '{$datetime}%'";
+        $money = $adminDB->ExecSQL($sqlMoney, $conn);
+        array_push($return['money'], $money[0]['money'] == null ? 0 : $money[0]['money']);
+
+        // set xAxis
+        array_push($return['xAxis'], $datetime);
+        }
+        } else {
+        $datetimeBegin = date('m', strtotime($beginTime));
+        $datetimeEnd = date('m', strtotime($endTime));
+        $months = $datetimeEnd - $datetimeBegin;
+        $year = date('Y', strtotime($beginTime));
+
+        for ($i = 0; $i <= $months; $i++) {
+        $datetime = $year . '-' . (($datetimeBegin + $i) <= 9 ? '0' . ($datetimeBegin + $i) : $datetimeBegin + $i);
+        // get time
+        $sqlTime = "select sum(time_len) as time from statistic_view where mer_id = {$mer_id}  and consume_t like '{$datetime}%'";
+        $time = $adminDB->ExecSQL($sqlTime, $conn);
+        array_push($return['time'], $time[0]['time'] == null ? 0 : $time[0]['time']);
+
+        // get money
+        $sqlMoney = "select sum(cost) as money from statistic_view where mer_id='{$mer_id}' and consume_t like '{$datetime}%'";
+        $money = $adminDB->ExecSQL($sqlMoney, $conn);
+        array_push($return['money'], $money[0]['money'] == null ? 0 : $money[0]['money']);
+
+        array_push($return['xAxis'], $datetime);
+        }
+        }
+         */
+
+        $ret['xAixs'] = ['周一','周二','周三','周四','周五','周六','周日'];
         // 查找逻辑， 未实现
 
-        // 测试数据
         $trend = [];
-        $trend[0] = ['media_type'=>'微博', 'data' => [120, 132, 101, 134, 90, 230, 210]];
-        $trend[1] = ['media_type'=>'微信', 'data' => [120, 132, 101, 134, 90, 230, 210]];
-        $trend[2] = ['media_type'=>'新闻', 'data' => [120, 132, 101, 134, 90, 230, 210]];
-        $trend[3] = ['media_type'=>'论坛', 'data' => [120, 132, 101, 134, 90, 230, 210]];
+        switch ($obj){
+            case 'media':{  // media trend
+                $trend[0] = ['media_type'=>'微博', 'data' => [120, 132, 101, 134, 90, 230, 210]];
+                $trend[1] = ['media_type'=>'微信', 'data' => [120, 132, 101, 134, 90, 230, 210]];
+                $trend[2] = ['media_type'=>'新闻', 'data' => [120, 132, 101, 134, 90, 230, 210]];
+                $trend[3] = ['media_type'=>'论坛', 'data' => [120, 132, 101, 134, 90, 230, 210]];
+                $ret['data'] = $trend;
+                break;
+            }
+            case 'public':{     // public trend
 
-        $ret['data'] = $trend;
+                $ret['data'] = $trend;
+                break;
+            }
+        }
+        $ret['params'] = $params;
         $this->jsonReturn($ret);
     }
 }
